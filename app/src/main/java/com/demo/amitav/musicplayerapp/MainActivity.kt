a6f1e1f4213609a7c48fab6d07e8e9aae46463b2
amitav.singh <singh.amitav26@gmail.com>
2022-08-17 09:55:39 +0200

    Dagger Hilt Setup

--- app/src/main/java/com/demo/amitav/musicplayerapp/MainActivity.kt
+++ app/src/main/java/com/demo/amitav/musicplayerapp/MainActivity.kt
@@ -1,24 +1,14 @@
 package com.demo.amitav.musicplayerapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.demo.amitav.musicplayerapp.data.remote.MusicDatabase
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GlobalScope.launch(Dispatchers.IO) {
-
-            val db = MusicDatabase()
-
-            db.getAllSongs()
-
-        }
     }
 }
---
+++ app/src/main/java/com/demo/amitav/musicplayerapp/MusicApplication.kt
@@ -0,0 +1,8 @@
+package com.demo.amitav.musicplayerapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MusicApplication : Application() {
+}
